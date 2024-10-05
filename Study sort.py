a25224a6cb8f521166e8c1c6ba8edde7d5f89575
emilh <emil.hasanli@student.kuleuven.be>
2024-10-05 19:19:19 +0200

    first commit

---
+++ Study sort.py
@@ -0,0 +1,56 @@
+import shutil
+import os
+from pathlib import Path
+
+#Alternating current grids, Operating systems, Digital Signal Processing, Electronic Design, Philosophy, Sensors and Actuaators
+Subjectlist = ["AC", "OS", "DE", "DSP", "ED", "PH", "SAA"] 
+
+#List of types Lecture, Lab, Exercise
+Typelist = ["Lec", "Lab", "Ex"]
+
+#Exception names have to be coded seperately
+exceptions = ["basics"]
+
+#Finds file in given directory
+def find_file(filename, search_path):
+    for root, dirs, files in os.walk(search_path):
+        if filename in files:
+            return os.path.join(root, filename)
+    return None
+
+#finds type of file cases can be added seperately
+def seperate_types(name):
+    for t in Typelist:
+        if t in name:
+            return t
+        else:
+            return "Lec"
+    
+#Moves file into sorted directory (filename, initial directory,  Course code)
+def move_file(filename, search_directory, s):
+    ftype = seperate_types(filename)
+    fpath = os.path.join(search_directory, filename) #initial filepath
+    cpath = os.path.join(r"C:\Users\emilh\OneDrive\Desktop\Year 3",s,ftype) #complete filepath
+    if not Path(os.path.join(r"C:\Users\emilh\OneDrive\Desktop\Year 3", s, ftype, filename)).exists():
+        shutil.move(fpath, cpath)
+        print(filename)
+        return
+
+#directory of download by default
+search_directory = r"C:\Users\emilh\Downloads"
+
+while True:
+    for filename in os.listdir(search_directory):
+        for s in Subjectlist:
+            if s in filename:
+                if "crdownload" not in filename:
+                    move_file(filename, search_directory, s)
+            
+        for e in exceptions:
+            if e in filename:
+                match e:
+                    case "basics":
+                        if "crdownload" not in filename:
+                            move_file(filename, search_directory, e)
+                    
+
